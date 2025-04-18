eda7c98694211d97cc6a294b54809dec56e8c344
Robuspy <robusra@gmail.com>
2025-04-17 20:43:23 -0400

    clientes codigo

--- WsRest/src/main/java/com/pablosrl/service/cuentas_cobrar/ClienteSaldoService.java
+++ WsRest/src/main/java/com/pablosrl/service/cuentas_cobrar/ClienteSaldoService.java
@@ -22,6 +22,8 @@ public class ClienteSaldoService {
             // Parámetros de entrada
             stmt.setString(1, codEmpresa);       // pvcod_empresa
             stmt.setString(2, codCliente);       // pccod_cliente
+            
+            stmt.registerOutParameter(2, Types.VARCHAR); // ← recibir posible cambio
 
             // Parámetros de salida
             stmt.registerOutParameter(3, Types.VARCHAR);       // pcnom_cliente
@@ -36,17 +38,18 @@ public class ClienteSaldoService {
 
             stmt.execute();
 
-            // Asignar valores al DTO
-            clienteSaldo.setCodCliente(codCliente);
-            clienteSaldo.setNombreCliente(stmt.getString(3));
-            clienteSaldo.setSaldoGs(getBigDecimalAsString(stmt.getBigDecimal(4)));
-            clienteSaldo.setLimiteCredito(getBigDecimalAsString(stmt.getBigDecimal(5)));
-            clienteSaldo.setPosibleCompra(getBigDecimalAsString(stmt.getBigDecimal(6)));
-            clienteSaldo.setChequesPendientes(getBigDecimalAsString(stmt.getBigDecimal(7)));
-            clienteSaldo.setSaldoFavor(getBigDecimalAsString(stmt.getBigDecimal(8)));
-            clienteSaldo.setNotasCredito(getBigDecimalAsString(stmt.getBigDecimal(9)));
-            clienteSaldo.setSaldoTotal(getBigDecimalAsString(stmt.getBigDecimal(10)));
-            clienteSaldo.setMensaje(stmt.getString(11));
+	         // Asignar valores al DTO desde los parámetros devueltos por el paquete
+	         clienteSaldo.setCodCliente(stmt.getString(2)); // ← importante: obtener valor real del paquete
+	         clienteSaldo.setNombreCliente(stmt.getString(3));
+	         clienteSaldo.setSaldoGs(getBigDecimalAsString(stmt.getBigDecimal(4)));
+	         clienteSaldo.setLimiteCredito(getBigDecimalAsString(stmt.getBigDecimal(5)));
+	         clienteSaldo.setPosibleCompra(getBigDecimalAsString(stmt.getBigDecimal(6)));
+	         clienteSaldo.setChequesPendientes(getBigDecimalAsString(stmt.getBigDecimal(7)));
+	         clienteSaldo.setSaldoFavor(getBigDecimalAsString(stmt.getBigDecimal(8)));
+	         clienteSaldo.setNotasCredito(getBigDecimalAsString(stmt.getBigDecimal(9)));
+	         clienteSaldo.setSaldoTotal(getBigDecimalAsString(stmt.getBigDecimal(10)));
+	         clienteSaldo.setMensaje(stmt.getString(11));
+
 
             // Logs para depuración
             System.out.println("Consulta Paquete de Saldo del Cliente - Limites Creditos:");
