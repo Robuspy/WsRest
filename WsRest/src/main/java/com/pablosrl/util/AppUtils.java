6db6718eb675732231db9df238bca926cfd59c35
Robuspy <robusra@gmail.com>
2025-04-20 19:19:31 -0400

    a

--- WsRest/src/main/java/com/pablosrl/util/AppUtils.java
+++ WsRest/src/main/java/com/pablosrl/util/AppUtils.java
@@ -18,12 +18,12 @@ public class AppUtils {
     static Logger logger = Logger.getLogger(AppUtils.class);
 
     // URLs de conexión a la base de datos para desarrollo y producción
-    private static final String DATABASE_URL = "jdbc:oracle:thin:@192.168.100.225:1521:ORCL"; // DESARROLLO
-  //private static final String DATABASE_URL = "jdbc:oracle:thin:@192.168.0.56:1521:ORCL";   // PRODUCCION
+    //private static final String DATABASE_URL = "jdbc:oracle:thin:@192.168.100.225:1521:ORCL"; // DESARROLLO
+    private static final String DATABASE_URL = "jdbc:oracle:thin:@192.168.0.56:1521:ORCL";   // PRODUCCION
 
     // Ruta de imágenes
-    public static final String IMAGE_DIRECTORY_DEV = "\\\\192.168.100.225\\fotos_articulos\\"; // Desarrollo
-    //public static final String IMAGE_DIRECTORY_DEV = "\\\\192.168.0.56\\inventiva\\INVENTIVA\\EXE\\FOTOS_ARTICULOS\\"; // Producción
+    //public static final String IMAGE_DIRECTORY_DEV = "\\\\192.168.100.225\\fotos_articulos\\"; // Desarrollo
+    public static final String IMAGE_DIRECTORY_DEV = "\\\\192.168.0.56\\inventiva\\INVENTIVA\\EXE\\FOTOS_ARTICULOS\\"; // Producción
     
     
     
@@ -42,8 +42,8 @@ public class AppUtils {
             config.setJdbcUrl(DATABASE_URL);
             // Credenciales por defecto para evitar excepciones en inicialización
             config.setUsername("inv");
-            //config.setPassword("openinv");
-            config.setPassword("inv"); //parar desarrollo
+            config.setPassword("openinv");
+            //config.setPassword("inv"); //parar desarrollo
 
         } catch (Exception  e) {
             e.printStackTrace();
